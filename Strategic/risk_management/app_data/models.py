6adb9f2fefefd698a8a4ae85ca9e9e822fc557ff
shurshun177 <artur.k177@gmail.com>
2018-10-31 17:29:58 +0200

    Update models.py

--- Strategic/risk_management/app_data/models.py
+++ Strategic/risk_management/app_data/models.py
@@ -1,53 +1,76 @@
-from django.db import models
-from .values import HOSPITAL_TYPES, VERSION_TYPES,\
-    ACTIVE_STATUS, BS_CODE, MEASURE_TYPES, FREQUENCY
+from djongo import models
 
 
 # Create your models here.
 class Version(models.Model):
-    version_number = models.IntegerField(unique=True, verbose_name='Version_No')
-    version_name = models.CharField(max_length=72, verbose_name='Version_Name')
-    version_type = models.CharField(max_length=1, choices=VERSION_TYPES,
-                                    verbose_name='Version_Types')
-
-    hospital_type = models.CharField(max_length=1, choices=HOSPITAL_TYPES,
-                                     verbose_name='Hospital_Type')
-    active = models.CharField(max_length=1, choices=ACTIVE_STATUS,
-                              verbose_name='Active')
-    #business_subject = models.ManyToManyField('BusinessSubject')
+    version_number = models.AutoField(unique=True, default=1000)
+    hospital_type = models.CharField(max_length=1)
+    version_name = models.CharField(max_length=72)
+    version_type = models.CharField(max_length=1)
+    active = models.CharField(max_length=1)
+    hospital_code = models.CharField()      #??????????
+    #business_topic = models.CharField()            ???????????
+    measure = object
     created_at = models.DateTimeField(auto_now_add=True, null=True,
                                       verbose_name='Created_At')
     updated_at = models.DateTimeField(verbose_name='Update_At')
-    measure = models.ManyToManyField('Measure')
+    create_user = models.CharField(max_length=28)
+    update_user = models.CharField(max_length=28)
+    cancel = models.BooleanField(max_length=1)
+    cancel_date = models.DateTimeField()
+    cancel_user = models.CharField(max_length=28)
 
+    def __str__(self):
+        return self.version_name
 class Measure(models.Model):
-    bs_code = models.CharField(max_length=1, choices=BS_CODE)
-    #version = models.ManyToManyField('Version')
-    measure_code = models.CharField(max_length=32, verbose_name='Measure_Code')
+    measure_code = models.CharField(unique=True, max_length=32)
+    hospital_type = models.CharField(max_length=1)
     measure_name = models.CharField(max_length=64,
                                     verbose_name='Measure_Name')
     measure_desc = models.TextField(max_length=200,
                                     verbose_name='Measure_Desc')
-    criteria_inclusion = models.CharField(max_length=72,
-                                          verbose_name='Criteria_Inclusion')
-    removal_criteria = models.CharField(max_length=72,
-                                        verbose_name='Removal_Criteria')
-    numerator = models.CharField(max_length=64,
-                                 verbose_name='Numerator')
-    denominator = models.CharField(max_length=64,
-                                   verbose_name='denominator')
-    hospital_type = models.CharField(max_length=1, choices=HOSPITAL_TYPES)
-    measure_type = models.CharField(max_length=64, choices=MEASURE_TYPES,
-                                    verbose_name='Measure_Type')
-    measuring_frequency = models.CharField(max_length=64,choices=FREQUENCY,
-                                           verbose_name='Measuring_Frequency')
-    unit_measure = models.CharField(max_length=64, verbose_name='Unit_Measure')
-    digit_num = models.IntegerField(verbose_name='Num_Of_Digits')
-    separate_thousands = models.BooleanField(max_length=1, verbose_name='Separate_Thousands')
-    active = models.BooleanField(max_length=1, choices=ACTIVE_STATUS,
-                              verbose_name='Active')
+    business_topic = models.CharField()
+    criteria_inclusion = models.CharField(max_length=72)
+    removal_criteria = models.CharField(max_length=72)
+    numerator = models.CharField(max_length=64)
+    denominator = models.CharField(max_length=64)
+    measure_type = models.CharField(max_length=64)
+    measuring_frequency = models.CharField(max_length=64)
+    unit_measure = models.CharField(max_length=1)
+    digit_num = models.IntegerField()
+    separate_thousands = models.BooleanField(max_length=1)
+    active = models.CharField(max_length=1, verbose_name='Active')
     from_date = models.DateTimeField(verbose_name='From_Date')
     to_date = models.DateTimeField(verbose_name='To_Date')
     target = models.CharField(max_length=64,verbose_name='Target')
-    remarks = models.TextField(max_length=484, blank=True, null=True,
-                               verbose_name='Remarks')
+    remarks = models.TextField(max_length=484, blank=True, null=True)
+    create_date = models.DateTimeField()
+    create_user = models.CharField(max_length=28)
+    change_date = models.DateTimeField()
+    change_user = models.CharField(max_length=28)
+    cancel = models.BooleanField(max_length=1)
+    cancel_date = models.DateTimeField()
+    cancel_user = models.CharField(max_length=28)
+    def __str__(self):
+        return self.measure_name
+
+class DecryptionTables(models.Model):
+    version_type =
+    #hospital_type =
+    #measure_type =
+    #active_status =
+    #business_topic =
+    #measure_frequency =
+
+class ActualExecution(models.Model):
+    version_number = models.IntegerField()
+    measure_code = models.CharField(max_length=32)
+    hospital_type = models.CharField(max_length=1)
+    measure_value = object
+    create_date = models.DateTimeField()
+    create_user = models.CharField(max_length=28)
+    change_date = models.DateTimeField()
+    change_user = models.CharField(max_length=28)
+    cancel = models.BooleanField(max_length=1)
+    cancel_date = models.DateTimeField()
+    cancel_user = models.CharField(max_length=28)
