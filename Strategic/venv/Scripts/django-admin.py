e8283b6c013671f253da3b441322654ff6992481
shurshun177 <artur.k177@gmail.com>
2018-11-07 17:47:28 +0200

    Delete django-admin.py

--- Strategic/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\artur.shalumov\PycharmProjects\Strategic\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
